d7c963298f94412a79937eb50d5a02b87ba10622
budgerigar <kii5800yahoo.co.jp>
2023-10-19 15:45:14 +0900

    main

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: "https://sample-app-3da47-default-rtdb.firebaseio.com",
   projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
+  measurementId: "G-4TVWCY3S3F"
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+getAnalytics(app)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
